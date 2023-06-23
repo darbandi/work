cfc5c622d19a5a86fa30f246249a27cdbe10797e
Mohammad Darbandi <Darbandi.mohamadhosein@Hasti.co>
2023-06-23 06:51:36 +0330

    feat: the pwa was added

--- next.config.js
+++ next.config.js
@@ -1,28 +1,35 @@
 const { parsed: localEnv } = require('dotenv').config()
 
-module.exports = () => {
-  const nextConfig = {
-    env: {
-      ...localEnv,
-    },
-    i18n: {
-      localeDetection: false,
-      locales: ['fa', 'en'],
-      defaultLocale: 'fa',
-    },
-    trailingSlash: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '3000',
-        },
-      ],
-    },
-    compiler: {
-      styledComponents: true,
-    },
-  }
-  return nextConfig
-}
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // Disable the caching of images since we use dynamic image imports
+  // disable: process.env.NODE_ENV === 'development',
+  // Register the ServiceWorker
+  register: true,
+  // Define the scope of the ServiceWorker
+  skipWaiting: true,
+})
+
+module.exports = withPWA({
+  env: {
+    ...localEnv,
+  },
+  i18n: {
+    localeDetection: false,
+    locales: ['fa', 'en'],
+    defaultLocale: 'fa',
+  },
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+      },
+    ],
+  },
+  compiler: {
+    styledComponents: true,
+  },
+})
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,72 @@ export default class CustomDocument extends Document {
   render(): React.JSX.Element {
     return (
       <Html>
-        <Head>{this.props.styles}</Head>
+        <Head>
+          <meta charSet='utf-8' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+          />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='apple-touch-icon' href='/icons/icon-72x72.png' />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='48x48'
+            href='/icons/icon-48x48.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='72x72'
+            href='/icons/icon-72x72.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='96x96'
+            href='/icons/icon-96x96.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='128x128'
+            href='/icons/icon-128x128.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='144x144'
+            href='/icons/icon-144x144.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='152x152'
+            href='/icons/icon-152x152.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='192x192'
+            href='/icons/icon-192x192.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='384x384'
+            href='/icons/icon-384x384.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='512x512'
+            href='/icons/icon-512x512.png'
+          />
+          <meta name='theme-color' content='#fff' />
+          {this.props.styles}
+        </Head>
         <body>
           <Main />
           <NextScript />
